b3b080caec37cef2b87f2d331be80a59ff91aa72
Aseer KT <aseermiqdad@gmail.com>
2021-07-13 20:21:12 +0530

    feat: create channel mutation

--- server/resolvers/channel.resolver.js
+++ server/resolvers/channel.resolver.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  Mutation: {
+    async createChannel(root, args, { db }) {
+      try {
+        await db.channel.create(args);
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+  },
+};
