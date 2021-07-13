df745cecf475782681e8a2a2a03c9756bd7e87ed
Aseer KT <aseermiqdad@gmail.com>
2021-07-13 20:17:30 +0530

    feat: create team mutation

--- server/resolvers/team.resolver.js
+++ server/resolvers/team.resolver.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  Mutation: {
+    async createTeam(root, args, { db }) {
+      try {
+        await db.team.create(args);
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+  },
+};
