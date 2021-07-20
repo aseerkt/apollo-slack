d359c199211a56eaf741a5bee13d7f2b108995c5
Aseer KT <aseermiqdad@gmail.com>
2021-07-20 12:04:17 +0530

    redirect to first channel on undefined channelId

--- client/src/hooks/useGetTeamInfo.jsx
+++ client/src/hooks/useGetTeamInfo.jsx
@@ -1,12 +1,16 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useGetTeamQuery from './apollo/queries/getTeam';
 
 export default function useGetTeamInfo() {
+  const history = useHistory();
   const { teamId, channelId } = useParams();
   const { data, loading } = useGetTeamQuery({
     variables: { teamId: parseInt(teamId) },
     skip: typeof teamId !== 'string',
   });
+
+  if (!channelId && data)
+    history.push(`/client/T${teamId}/C${data?.getTeam?.channels[0].id}`);
 
   const currentChannel = data?.getTeam?.channels?.find(
     (c) => c.id === parseInt(channelId),
