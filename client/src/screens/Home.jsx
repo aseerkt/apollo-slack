34e3727faaa264582d456bfd10b9f08cb885eddd
Aseer KT <aseermiqdad@gmail.com>
2021-07-24 20:13:23 +0530

    fix: render dashboard link only when logged in

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
-import { Spin } from 'antd';
-import useAllUsersQuery from '../hooks/apollo/queries/allUsers';
+
 import Logo from '../shared/Logo';
 
 export default function Home() {
+  const { data } = useMeQuery();
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <Logo color='dark' />
 
       <Link to='/register'>Register</Link>
       <Link to='/login'>Login</Link>
-      <Link to='/dashboard'>Dashboard</Link>
+      {data?.me && <Link to='/dashboard'>Dashboard</Link>}
     </div>
   );
 }
